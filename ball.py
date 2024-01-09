26bdda521ae96336b79a5df7b093e14835a4cfa8
Sakib_Dalal <sakibdalal73@gmail.com>
2024-01-09 23:00:45 +0530

    detect collision with paddle

--- ball.py
+++ ball.py
@@ -8,8 +8,18 @@ class Ball(turtle.Turtle):
         self.color("white")
         self.shape("circle")
         self.shapesize(1, 1)
+        self.x_move = 10
+        self.y_move = 10
+        self.x_pos = 0
+        self.y_pos = 0
 
     def move(self):
-        self.x_pos = self.xcor() + 10
-        self.y_pos = self.ycor() + 10
+        self.x_pos = self.xcor() + self.x_move
+        self.y_pos = self.ycor() + self.y_move
         self.goto(self.x_pos, self.y_pos)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
--- main.py
+++ main.py
@@ -27,4 +27,13 @@ while is_game_on:
     ball.move()
     screen.update()
 
+    # Detect collision with ball and wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision of ball from paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() > -320:
+        ball.bounce_x()
+        
+
 screen.exitonclick()
